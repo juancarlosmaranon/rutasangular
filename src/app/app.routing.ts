834e22059cdf6313c13d95a7e027f608681d209b
juancarlosmaranon <juancar2001jcmg@gmail.com>
2022-11-14 12:42:50 +0100

    kl

---
+++ src/app/app.routing.ts
@@ -0,0 +1,27 @@
+import { HomeComponent } from "./components/home/home.component";
+import { CineComponent } from "./components/cine/cine.component";
+import { MusicaComponent } from "./components/musica/musica.component";
+import { TelevisionComponent } from "./components/television/television.component";
+import { MenuComponent } from "./components/menu/menu.component";
+
+//NECESITAMOS LOS SIGUIENTES MODULOS PARA REALIZAR ROUTING
+import { Routes, RouterModule } from "@angular/router";
+import {ModuleWithProviders} from "@angular/core";
+import { Error404Component } from "./components/error404/error404.component";
+import { NumerodobleComponent } from "./components/numerodoble/numerodoble.component";
+
+//DEFINIMOS UN ARRAY DE OBJETOS Routes
+const appRoutes: Routes = [
+    {path:"",component: HomeComponent},
+    {path:"musica", component:MusicaComponent},
+    {path:"cine", component:CineComponent},
+    {path:"television",component:TelevisionComponent},
+    {path:"menu",component:MenuComponent},
+    {path:"**",component:Error404Component},
+    {path:"numerodoble/:numero",component:NumerodobleComponent}
+]
+
+//DE ESTA CLASE DEBEMOS EXPORTAR DOS ELEMENTOS PARA app.module.ts
+export const appRoutingProviders: any[] =[];
+export const routing: ModuleWithProviders<any>=
+RouterModule.forRoot(appRoutes);
---
+++ src/app/components/numerodoble/numerodoble.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-numerodoble',
+  templateUrl: './numerodoble.component.html',
+  styleUrls: ['./numerodoble.component.css']
+})
+export class NumerodobleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
